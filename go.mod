0bd05826d62b8d83d713de9b2fc234f39e3f83f8
Dragan Milic <dragan@netice9.com>
2020-09-13 23:10:44 +0100

    add prometheus metrics change listener

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.7.1
 	github.com/stretchr/testify v1.6.1
 	go.etcd.io/bbolt v1.3.5
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
---
+++ metrics/metrics_change_listener.go
@@ -0,0 +1,89 @@
+package metrics
+
+import (
+	"github.com/draganm/bolted"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type metricsChangeListener string
+
+var numberOfWriteTransactionsVec = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "bolted_number_of_write_transactions_total",
+	Help: "Total number of write transactions performed on the database",
+}, []string{
+	"dbname",
+})
+
+var numberOfSuccessfulWriteTransactionsVec = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "bolted_number_of_write_transactions_successful",
+	Help: "Number of successful write transactions performed on the database",
+}, []string{
+	"dbname",
+})
+
+var numberOfFailedTransactionsVec = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "bolted_number_of_write_transactions_failed",
+	Help: "Number of failed write transactions performed on the database",
+}, []string{
+	"dbname",
+})
+
+func (l metricsChangeListener) Opened(b *bolted.Bolted) error {
+	return nil
+}
+
+func (l metricsChangeListener) Start(w bolted.WriteTx) error {
+	dbname := string(l)
+
+	cnt, err := numberOfWriteTransactionsVec.GetMetricWithLabelValues(dbname)
+	if err != nil {
+		return errors.Wrap(err, "while getting metric counter")
+	}
+
+	cnt.Add(1)
+	return nil
+}
+
+func (l metricsChangeListener) Delete(w bolted.WriteTx, path string) error {
+	return nil
+}
+
+func (l metricsChangeListener) CreateMap(w bolted.WriteTx, path string) error {
+	return nil
+}
+
+func (l metricsChangeListener) Put(w bolted.WriteTx, path string, newValue []byte) error {
+	return nil
+}
+
+func (l metricsChangeListener) BeforeCommit(w bolted.WriteTx) error {
+	return nil
+}
+
+func (l metricsChangeListener) AfterTransaction(err error) error {
+	dbname := string(l)
+	if err != nil {
+
+		cnt, err := numberOfFailedTransactionsVec.GetMetricWithLabelValues(dbname)
+		if err != nil {
+			return errors.Wrap(err, "while getting metric counter")
+		}
+		cnt.Inc()
+	} else {
+		cnt, err := numberOfSuccessfulWriteTransactionsVec.GetMetricWithLabelValues(dbname)
+		if err != nil {
+			return errors.Wrap(err, "while getting metric counter")
+		}
+		cnt.Inc()
+	}
+	return nil
+}
+
+func (l metricsChangeListener) Closed(b *bolted.Bolted) error {
+	dbname := string(l)
+	numberOfWriteTransactionsVec.DeleteLabelValues(dbname)
+	numberOfSuccessfulWriteTransactionsVec.DeleteLabelValues(dbname)
+	numberOfFailedTransactionsVec.DeleteLabelValues(dbname)
+	return nil
+}
