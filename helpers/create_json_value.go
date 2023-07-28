0eebf9f9590641968c0b5013bd131dcf002d90f3
Dragan Milic <dragan@netice9.com>
2023-07-28 23:24:13 +0200

    documented create_json_value function

--- helpers/create_json_value.go
+++ helpers/create_json_value.go
@@ -1,3 +1,4 @@
+// Package helpers provides utility functions to work with bolted DB.
 package helpers
 
 import (
@@ -8,6 +9,17 @@ import (
 	"github.com/draganm/bolted/dbpath"
 )
 
+// CreateJSONValue is a utility function that creates, initializes, and stores a JSON representation of a value
+// at a specified path within a write transaction. The value is of type T, instantiated within the function, and
+// its initialization is handled by a callback function provided by the caller.
+//
+// Parameters:
+//   - tx: The write transaction within which the operation is performed. It must be active and not nil.
+//   - pth: The dbpath.Path instance where the JSON value is to be stored within the transaction. It must be a valid path.
+//   - fn: A callback function provided by the caller to initialize the value. It accepts an instance of the type *T
+//
+// Returns:
+// An error if there was an issue with creating the value or marshalling it to JSON. Otherwise, it returns nil.
 func CreateJSONValue[T any](tx bolted.WriteTx, pth dbpath.Path, fn func(v *T) error) error {
 
 	v := new(T)
